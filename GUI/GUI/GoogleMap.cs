beb160cafccb55acf011e6ab8622eda215d089db
Toni Stoyanov <tohnkz@abv.bg>
2019-12-10 14:46:12 +0200

    WPF user interface

---
+++ GUI/GUI/GoogleMap.cs
@@ -0,0 +1,43 @@
+﻿using GMap.NET.WindowsPresentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public class GoogleMap : GMapMarker
+    {
+        public  GMapControl GMap { get; private set; }
+        public GoogleMap(GMapControl map) : base(new GMap.NET.PointLatLng(23, 23))
+        {
+            GMap = map;
+        }
+        
+        public void InitMap()
+        {
+
+            //GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            //MainWindow map.CanDragMap = true;
+            //map.Zoom = 5;
+            //map.MaxZoom = 18;
+            //map.MinZoom = 0;
+
+            //double lat = 23;
+            //double lng = 23;
+            //map.MapProvider = GMapProviders.GoogleMap;
+            //map.Position = new PointLatLng(lat, lng);
+
+            //PointLatLng point = new PointLatLng(lat, lng);
+            //GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(-25.966688, 32.580528),
+            //                                           GMarkerGoogleType.green);
+
+
+            //GMapMarker marker = new GMapMarker(point);
+            //markers.Markers.Add(marker);
+
+            // map.Overlays.Add(markers);
+        }
+    }
+}
--- GUI/GUI/MainWindow.xaml.cs
+++ GUI/GUI/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using GMap.NET;
 using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
-using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsPresentation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,16 +26,60 @@ namespace GUI
     {
         public MainWindow()
         {
-            map.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
+            
             InitializeComponent();
-            map.MapProvider = GMapProviders.GoogleMap;
-            map.Position= new PointLatLng(23, 23);
 
-            GMapOverlay oMarker = new GMapOverlay("marker1");
-            GMarkerGoogle marker = new GMarkerGoogle(new PointLatLng(-25.966688, 32.580528),
-            GMarkerGoogleType.green);
-            oMarker.Markers.Add(marker);
-            //map.OverLays
+            InitMap();
+            InitViewAllList(dataGrid);
+            
+        }
+
+        public void InitMap()
+        {
+            //GoogleMap gMap = new GoogleMap(map);
+            //GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            //map.CanDragMap = true;
+            //map.Zoom = 5;
+            //map.MaxZoom = 18;
+            //map.MinZoom = 0;
+
+            //double lat = 23;
+            //double lng = 23;
+            //map.MapProvider = GMapProviders.GoogleMap;
+            //map.Position = new PointLatLng(lat, lng);
+
+            //PointLatLng point = new PointLatLng(lat, lng);
+            //GMap.NET.WindowsForms.Markers.GMarkerGoogle marker1 = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(new PointLatLng(-25.966688, 32.580528),
+            //                                           GMap.NET.WindowsForms.Markers.GMarkerGoogleType.green);
+
+
+            ////GMapMarker marker = new GMapMarker(point);
+            ////map.Markers.Add(marker);
+
+            ////GMap.NET.WindowsForms.GMapOverlay markers = new GMap.NET.WindowsForms.GMapOverlay("marker");
+            ////map.Over
+        }
+
+        public void InitViewAllList(DataGrid dataGrid)
+        {
+            List<dynamic> it = new List<dynamic>();
+            it.Add(new { no = 1, name = "sensor1", desc = "asdf", curValue = 23, graphics = "some URL1" });
+            it.Add(new { no = 2, name = "sensor2", desc = "asdf", curValue = 24, graphics = "some URL2" });
+            it.Add(new { no = 3, name = "sensor3", desc = "asdf", curValue = 25, graphics = "some URL3" });
+            it.Add(new { no = 4, name = "sensor4", desc = "asdf", curValue = 26, graphics = "some URL4" });
+            it.Add(new { no = 5, name = "sensor5", desc = "asdf", curValue = 27, graphics = "some URL5" });
+            it.Add(new { no = 6, name = "sensor6", desc = "asdf", curValue = 28, graphics = "some URL6" });
+            it.Add(new { no = 7, name = "sensor5", desc = "asdf", curValue = 27, graphics = "some URL5" });
+            it.Add(new { no = 8, name = "sensor6", desc = "asdf", curValue = 28, graphics = "some URL6" });
+            it.Add(new { no = 9, name = "sensor5", desc = "asdf", curValue = 27, graphics = "some URL5" });
+            it.Add(new { no = 0, name = "sensor6", desc = "asdf", curValue = 28, graphics = "some URL6" });
+            dataGrid.ItemsSource = it;
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            AddSensorWindow addSensorWindow = new AddSensorWindow();
+            addSensorWindow.ShowDialog();
         }
     }
 }
